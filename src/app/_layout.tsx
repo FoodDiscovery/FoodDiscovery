df06fec46eb9f39ef9f46c32e8922c392ce1c1eb
Alexander <bsantiagoalex@gmail.com>
2026-01-27 12:40:19 -0800

    have login component rendering

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,12 @@
+import AuthProvider from '../components/Auth';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-    </Stack>
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+      </Stack>
+    </AuthProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import { View, Button } from "react-native";
+// import React from "react";
+// import { Link } from "expo-router";
 
-const App = () => {
+// const index = () => {
+
+//   return (
+//     <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+//       <Link href={"/(auth)"} asChild>
+//         <Button title="Sign in" />
+//       </Link>
+//     </View>
+//   );
+// };
+
+// export default index;
+
+// import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '../lib/supabase'
+// import Auth from '\(auth\)/sign-in'
+import { View, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <View>
-      <Text>Hello World!</Text>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
     </View>
-  );
-};
-
-export default App;
+  )
+}
